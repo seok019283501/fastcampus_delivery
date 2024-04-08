12384a93553a6a8dc2c37a647487fb3c62081d35
minseok <alsthr0338@gmail.com>
2024-04-08 15:21:52 +0900

    배달플랫폼 api 기본 설정 추가 - 1

--- api/src/main/java/com/delivery/api/account/AccountApiController.java
+++ api/src/main/java/com/delivery/api/account/AccountApiController.java
@@ -1,5 +1,6 @@
 package com.delivery.api.account;
 
+import com.delivery.api.account.model.AccountMeResponse;
 import com.delivery.db.account.AccountEntity;
 import com.delivery.db.account.AccountRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,15 +8,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/account")
 public class AccountApiController {
     private final AccountRepository accountRepository;
-    @GetMapping("")
-    public void save(){
-        var account = AccountEntity.builder().build();
-        accountRepository.save(account);
+    @GetMapping("/me")
+    public AccountMeResponse save(){
+        return AccountMeResponse.builder()
+                .name("홍길동")
+                .email("A@email.com")
+                .registeredAt(LocalDateTime.now())
+                .build();
 
     }
 
---
+++ api/src/main/java/com/delivery/api/account/model/AccountMeResponse.java
@@ -0,0 +1,17 @@
+package com.delivery.api.account.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class AccountMeResponse {
+    private String email;
+    private String name;
+    private LocalDateTime registeredAt;
+}
