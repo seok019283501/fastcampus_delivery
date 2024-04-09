2e7d2dcbb4ca7090dba9a8428668c70819d6a4a8
minseok <alsthr0338@gmail.com>
2024-04-09 15:22:58 +0900

    api 공통 spec 적용하기

--- api/src/main/java/com/delivery/api/account/AccountApiController.java
+++ api/src/main/java/com/delivery/api/account/AccountApiController.java
@@ -1,6 +1,7 @@
 package com.delivery.api.account;
 
 import com.delivery.api.account.model.AccountMeResponse;
+import com.delivery.api.common.api.Api;
 import com.delivery.db.account.AccountEntity;
 import com.delivery.db.account.AccountRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,12 +17,13 @@ import java.time.LocalDateTime;
 public class AccountApiController {
     private final AccountRepository accountRepository;
     @GetMapping("/me")
-    public AccountMeResponse save(){
-        return AccountMeResponse.builder()
+    public Api<AccountMeResponse> me(){
+        var res = AccountMeResponse.builder()
                 .name("홍길동")
                 .email("A@email.com")
                 .registeredAt(LocalDateTime.now())
                 .build();
+        return Api.OK(res);
 
     }
 
