05f3a6c744e65dc3ab3b2f8bb3fa7b62661d01db
minseok <alsthr0338@gmail.com>
2024-04-09 20:31:43 +0900

    UserEntity 개발

--- db/src/main/java/com/delivery/db/BaseEntity.java
+++ db/src/main/java/com/delivery/db/BaseEntity.java
@@ -4,13 +4,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @MappedSuperclass
 @Data
 @SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ db/src/main/java/com/delivery/db/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.delivery.db.user;
+
+import com.delivery.db.user.enums.UserStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findFirstByIdAndStatusOrderByIdDesc(Long userId, UserStatus status);
+
+    Optional<UserEntity> findFirstByEmailAndPasswordAndStatusOrderByIdDesc(String Email, String password,UserStatus status);
+}
